1194297b2f72475f35484f7a619f2275f17f541c
anqz <aaronquilez@gmail.com>
2023-02-16 13:26:40 +0100

    Add new files and functionalities to repo
    Signed-off-by: Aaron Quilez
    Signed-off-by: anqz <aaronquilez@gmail.com>

--- src/main/java/com/example/demo/WhateverIWant.java
+++ src/main/java/com/example/demo/WhateverIWant.java
@@ -1,15 +1,34 @@
 package com.example.demo;
 
+import com.example.demo.domain.Player;
+import com.example.demo.domain.Team;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 @Controller
 public class WhateverIWant {
-    
+
+    private Team team;
+
+    @PostConstruct
+    public void init() {
+
+        Set<Player> players = new HashSet<>();
+        players.add(new Player("Charlie Brown", "pitcher"));
+        players.add(new Player("Snoopy", "shortstop"));
+
+        team = new Team("Peanuts", "California", players);
+    }
+
     @RequestMapping("/hi")
-    public @ResponseBody String hiThere() {
-        return "hello world!";
+    public @ResponseBody Team hiThere() {
+        return team;
     }
 
 }
